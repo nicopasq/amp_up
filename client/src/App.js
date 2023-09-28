a2e7f8bcc7453ea9005d19d17f20cceb1deff443
Nico <nicopasquariello@gmail.com>
2023-09-27 20:03:30 -0600

    Routes work, header does not route yet.

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+import Header from "./Header";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App(){
+return (
+    <>
+        <div>
+            <Header/>
+            <Switch>
+            <Route exact path="/my_home">
+                <h1>Welcome to my house!</h1>
+            </Route>
+            </Switch>
+            
+        </div>
+    </>
+);
 }
 
 export default App;
---
+++ client/src/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Header(){
+
+    return (
+        <div style={{backgroundColor: "Grey", position: "relative", top: "-2.21vh", right: "0.5vw", height: "7vh", width: "100vw"}}>
+            <h1>Click Me!</h1>
+        </div>
+    )
+}
+
+export default Header;
