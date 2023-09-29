7c61140b053d9c12de126032ee2f50054ddf665f
Nico <nicopasquariello@gmail.com>
2023-09-29 13:50:46 -0600

    Render build failed, removed Home page.

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -2,16 +2,12 @@ import React from "react";
 import { Route, Switch } from "react-router-dom";
 import Login from "./Login";
 import Signup from "./Signup";
-import Home from "./Home";
 
 function App(){
 return (
     <Switch>
         <Route path = "/signup">
             <Signup/>
-        </Route>
-        <Route path = "/home">
-            <Home/>
         </Route>
         <Route path = "/">
             <Login/>
