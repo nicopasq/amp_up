67d593895d83d6bd0c9410d41f0876cd03f6d874
Nico <nicopasquariello@gmail.com>
2023-09-28 19:05:01 -0600

    Added route on App.js to Login.js

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,7 +1,15 @@
 import React from "react";
+import { Route, Switch } from "react-router-dom";
+import Login from "./Login";
 
 function App(){
-    return null
+return (
+    <Switch>
+        <Route exact path = "/">
+            <Login/>
+        </Route>
+    </Switch>
+);
 }
 
 export default App;
