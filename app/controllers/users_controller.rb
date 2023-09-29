6d7827c8638f57293a4172abe2012e597c3bfbec
Nico <nicopasquariello@gmail.com>
2023-09-29 10:25:09 -0600

    Users can be created, passords are not protected yet

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,19 @@
 class UsersController < ApplicationController
+rescue_from ActiveRecord::RecordInvalid with: :invalidUserCreation
     wrap_parameters format: []
 
     def create
-        new_user = User.create(userParams)
-        new_user ? render json: new_user : render json: "Could not create account"
+        new_user = User.create!(userParams)
+        render json: new_user, status: :created
     end
 
     private
 
     def userParams
-        params.permit(:username, :password_digest)
+        params.permit(:name, :password_digest)
+    end
+
+    def invalidUserCreation invalid
+        render json: {errors: invalid.record.errors.full_messages }, status: 422
     end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ApplicationRecord
+    validates :name, presence: true
+    validates :password_digest, presence: true
 end
