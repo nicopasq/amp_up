98008a46bf2c2d8dcf9c86838fb6eed482ee57a2
Nico <nicopasquariello@gmail.com>
2023-09-28 19:33:46 -0600

    added some style to Login page

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -5,37 +5,70 @@ function Login(){
     const [usernameVal, setUsernameVal] = useState('')
     const [passwordVal, setPasswordVal] = useState('')
 
+
+const boxSX = {
+    width: '50%',
+    position: 'relative',
+    top: '20vh',
+    left: '22%',
+    // border: '1px solid black'
+}
+
+const loginFormStyle= {
+    // border: '1px solid orange',
+    width: 'fit-content',
+    position: 'relative',
+    left: '11vw'
+}
+
+const inputStyle = {
+    marginTop: '2vh',
+    
+}
+
+const submitBtnStyle = {
+    margin: "2vw", 
+    width:'100%', 
+    position: "relative", 
+    right: "2vw"
+}
+
     return (
         <Container>
-            <Box>
-            <Typography variant="h4">Login</Typography>
+            <Box sx={boxSX}>
+            <Typography sx={{
+                position: 'relative',
+                left: '43%'}}
+                variant="h4">Login</Typography>
 
-            <form id="loginForm">
-                    <Grid item xs={4}>
-                        <TextField 
-                        variant="standard" 
+            <form id="loginForm" style={loginFormStyle}>
+                    <Grid item xs={4} sx={inputStyle}>
+                        <TextField
+                        sx={{scale:"1.3"}} 
+                        variant="standard"
                         placeholder="Username"
                         onChange={(e) => setUsernameVal(e.target.value)}
                         value={usernameVal}/>
                     </Grid>
-                    <Grid item xs={4}>
+                    <Grid item xs={4} sx={inputStyle}>
                         <TextField 
+                        sx={{scale:"1.3"}} 
                         variant="standard" 
                         placeholder="Password"
                         onChange={(e) => setPasswordVal(e.target.value)}
                         value={passwordVal}/>
                     </Grid>
-                <Button variant="contained"
-                onClick={() => {
-                    console.log(
-                        `Log in user: 
-                        username: ${usernameVal}, 
-                        password: ${passwordVal}`)} 
-                    }>Login!</Button>
+                <Button 
+                sx={submitBtnStyle} 
+                variant="contained"
+                onClick={() => console.log(`Log in user: username: ${usernameVal}, password: ${passwordVal}`) }>Login!</Button>
             </form>
-
-            <Typography variant="body1">Don't Have an Account?</Typography>
-            <Typography variant="h4" onClick={() => console.log("Sign Me UP!")} >Sign Up</Typography>
+            <Typography sx={{position:'relative', left:'10.65vw'}} variant="body1">Don't Have an Account?</Typography>
+            <Typography sx={{
+                position: 'relative',
+                left: '40%'}} 
+                variant="h4"
+                onClick={() => console.log("Sign Me UP!")} >Sign Up</Typography>
 
             </Box>
         </Container>
