f0cffe6fd49bdbf0527c908fc293fb71a396e342
Nico <nicopasquariello@gmail.com>
2023-09-28 19:24:03 -0600

    Added a basic form to login.js

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,7 +1,29 @@
+import { Box, Button, Container, Grid, TextField, Typography } from "@mui/material";
 import React from "react";
 
 function Login(){
-    return null
+    return (
+        <Container>
+            <Box>
+            <Typography variant="h4">Login</Typography>
+
+            <form id="loginForm">
+                    <Grid item xs={4}>
+                        <TextField variant="standard" placeholder="Username"/>
+                    </Grid>
+                    <Grid item xs={4}>
+                        <TextField variant="standard" placeholder="Password"/>
+                    </Grid>
+                <Button variant="contained">Login!</Button>
+            </form>
+
+            <Typography variant="body1">Don't Have an Account?</Typography>
+            <Typography variant="h4">Sign Up</Typography>
+
+            </Box>
+        </Container>
+    )
 }
+
 
 export default Login;
