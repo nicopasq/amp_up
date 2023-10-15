197caaebe4ca454b6fb9ba2bc20044018d6f56ff
Nico <nicopasquariello@gmail.com>
2023-10-14 18:56:49 -0600

    Update and Edit btns are only visible on responses created by the user.:

--- client/src/components/ResponseDisplay.js
+++ client/src/components/ResponseDisplay.js
@@ -1,13 +1,22 @@
-import React from 'react'
+import React, { useContext, useState } from 'react'
 import '../styles/responseDisplay.css'
 import { Button, Typography } from '@mui/material'
+import { UserContext } from './UserContext'
 
 function ResponseDisplay({r}){
+    const { currentUser } = useContext(UserContext)
+    const visibility = {
+        visibility:"hidden"
+    }
+    const [btnDisplay, setBtnDisplay] = useState(visibility)
 
+    if (currentUser.id === r.user.id){
+        visibility.visibility = "block"
+    }
 return (
     <div className='responseContainer'>
         <Typography variant='h5'><u>{r.user.username}</u></Typography>
-        <div className='actionBtns'>
+        <div className='actionBtns' style={btnDisplay}>
             <Button variant='text'>✏️</Button>
             <Button variant='text'>🗑️</Button>
         </div>
