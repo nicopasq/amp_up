b2cd9c91cf88adefef0ad7a01ef7ef3a7c6c73ba
Nico <nicopasquariello@gmail.com>
2023-10-17 15:17:36 -0600

    Edit and Delete buttons are hidden unless the current user is the creator of the response.

--- client/src/components/ResponseDisplay.js
+++ client/src/components/ResponseDisplay.js
@@ -1,15 +1,22 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import '../styles/responseDisplay.css'
 import { Button, Typography } from '@mui/material'
+import { UserContext } from './UserContext'
 
 function ResponseDisplay({response}){
-
+    const {currentUser} = useContext(UserContext)
+    const buttonSx = {
+        visibility: 'hidden'
+    }
+    if (currentUser.id === response.user.id){
+        buttonSx.visibility = {visibility: 'block'}
+    }
     return (
         <li className='responseContainer' key={response.id}>
             <Typography variant='h5'><u>{response.user.username}</u></Typography>
             <div className='actionBtns'>
-                <Button variant='text'>✏️</Button>
-                <Button variant='text' onClick={()=> console.log('delete me')}>🗑️</Button>
+                <Button variant='text' sx={buttonSx}>✏️</Button>
+                <Button variant='text' sx={buttonSx} onClick={()=> console.log('delete me')}>🗑️</Button>
             </div>
             <Typography variant='h6'>{response.body}</Typography>
         </li>
